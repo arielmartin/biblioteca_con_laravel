22fd4704d0dec9110fd03c4e64c0108da85fc589
Ariel Martin <arielmartin@gmail.com>
2020-06-16 19:37:01 -0300

    se agrego jquery-validation

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -66,7 +66,11 @@
     <!-- AdminLTE App -->
     <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
 
-@yield("scripts")
+    <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
+    <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+    <script src="{{asset("assets/js/funciones.js")}}"></script>
+
+    @yield("scripts")
 
 </body>
 </html>
